ca14be2ea9fa12538b72707ad030ec481217611d
droidlinies <droidlinies1@gmail.com>
2021-08-02 23:27:07 +0100

    mobile update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,20 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-    }, 
+        extend: {
+            backgroundImage: {
+                1: "url('../assets/1a.jpeg')",
+                2: "url('../assets/2.jpeg')",
+                3: "url('../assets/3.jpg')",
+                4: "url('../assets/4.jpg')",
+                5: "url('../assets/5.jpg')"
+              },
+              flex: {
+                5: 5
+              }
+            }
+        },
+    
     variants: {
         extend: {},
     },
